af9fde853f25f7227c97d00366a4b8049143513b
minsuleefromnowhere <cw01094@gmail.com>
2025-01-04 23:32:17 +0900

    restaurantDTO 수정

--- src/main/java/com/human/hanmat/dto/RestaurantDTO.java
+++ src/main/java/com/human/hanmat/dto/RestaurantDTO.java
@@ -4,6 +4,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 public class RestaurantDTO {
@@ -11,14 +13,16 @@ public class RestaurantDTO {
     private String name;
     private String lmmAddr;
     private String roadAddr;
-    private boolean isClosed;
+    private Date regDate;
+    private String isClosed;
 
     @Builder
-    public RestaurantDTO(int id, String name, String lmmAddr, String roadAddr, boolean isClosed) {
+    public RestaurantDTO(int id, String name, String lmmAddr, String roadAddr,Date regDate, String isClosed) {
         this.id = id;
         this.name = name;
         this.lmmAddr = lmmAddr;
         this.roadAddr = roadAddr;
+        this.regDate = regDate;
         this.isClosed = isClosed;
     }
 }

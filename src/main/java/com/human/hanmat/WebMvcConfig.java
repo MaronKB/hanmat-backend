eea00b70744029a5ec466aa3b75d17db68e9091c
minsuleefromnowhere <cw01094@gmail.com>
2025-01-04 23:14:35 +0900

    portfolid? > portfolio?

--- src/main/java/com/human/hanmat/WebMvcConfig.java
+++ src/main/java/com/human/hanmat/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173", "https://portfolid.mrkb.kr")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173", "https://portfolio.mrkb.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)

72e0c62648f98f0b0030b5f120bd2df965f89992
minsuleefromnowhere <cw01094@gmail.com>
2025-01-12 01:41:43 +0900

    관리자 식당 검색이 안돼, 희철쿤 도와줘2

--- src/main/java/com/human/hanmat/repository/RestaurantRepository.java
+++ src/main/java/com/human/hanmat/repository/RestaurantRepository.java
@@ -1,6 +1,7 @@
 package com.human.hanmat.repository;
 
 import com.human.hanmat.entity.Restaurant;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -15,4 +16,30 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     @Query(value = "SELECT * FROM T_RESTAURANT WHERE RESTAURANT_Y > ?1 AND RESTAURANT_Y < ?2 AND RESTAURANT_X > ?3 AND RESTAURANT_X < ?4", nativeQuery = true)
     List<Restaurant> findByLocation(double minY, double maxY, double minX, double maxX);
+
+//    검색
+    @Query(value = "SELECT * FROM T_RESTAURANT WHERE LOWER(:fieldName) LIKE LOWER(CONCAT('%', :keyword, '%')) ORDER BY RESTAURANT_ID ASC OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY", nativeQuery = true)
+    List<Restaurant> findByField(@Param("fieldName") String fieldName,
+                                 @Param("keyword") String keyword,
+                                 @Param("offset") int offset,
+                                 @Param("limit") int limit);
+
+    @Query(value = "SELECT COUNT(*) FROM T_RESTAURANT WHERE LOWER(:fieldName) LIKE LOWER(CONCAT('%', :keyword, '%'))", nativeQuery = true)
+    int countByField(@Param("fieldName") String fieldName, @Param("keyword") String keyword);
+
+    @Query(value = "SELECT * FROM T_RESTAURANT WHERE RESTAURANT_REG_DATE LIKE CONCAT('%', :keyword, '%') ORDER BY RESTAURANT_ID ASC OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY", nativeQuery = true)
+    List<Restaurant> findByRegDate(@Param("keyword") String keyword,
+                                   @Param("offset") int offset,
+                                   @Param("limit") int limit);
+
+    @Query(value = "SELECT COUNT(*) FROM T_RESTAURANT WHERE RESTAURANT_REG_DATE LIKE CONCAT('%', :keyword, '%')", nativeQuery = true)
+    int countByRegDate(@Param("keyword") String keyword);
+
+    @Query(value = "SELECT * FROM T_RESTAURANT WHERE RESTAURANT_IS_CLOSED = :closed ORDER BY RESTAURANT_ID ASC OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY", nativeQuery = true)
+    List<Restaurant> findByClosed(@Param("closed") String closed,
+                                  @Param("offset") int offset,
+                                  @Param("limit") int limit);
+
+    @Query(value = "SELECT COUNT(*) FROM T_RESTAURANT WHERE RESTAURANT_IS_CLOSED = :closed", nativeQuery = true)
+    int countByClosed(@Param("closed") String closed);
 }

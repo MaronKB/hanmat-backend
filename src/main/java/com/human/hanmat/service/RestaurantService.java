5673a11d0d87978da0385a6bfde2b5afec3bed4c
minsuleefromnowhere <cw01094@gmail.com>
2025-01-20 15:43:31 +0900

    111

--- src/main/java/com/human/hanmat/service/RestaurantService.java
+++ src/main/java/com/human/hanmat/service/RestaurantService.java
@@ -84,7 +84,7 @@ public class RestaurantService {
     }
 
 
-//    검색
+//  관리자 검색
     @Transactional(readOnly = true)
     public List<RestaurantDTO> searchByCategory(String category, String keyword, int page, int size) {
         int startRow = (page - 1) * size + 1;
